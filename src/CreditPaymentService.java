0202da0a2a46cac75d1971005012ac05bef2e74c
AnastasiyaPod <an_pod12@mail.ru>
2023-08-26 16:44:58 +1000

    Correction

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public long calculate(long amountOfCredit, long timeMonth, double loanRate) {
-        double interestRate = loanRate / (100 * timeMonth);
+        double interestRate = loanRate / (100 * 12);
         double payment = amountOfCredit * (interestRate / (1 - Math.pow(1 + interestRate,
                 -timeMonth)));
         return (long) payment;
